cabd361ee39ffd0d862706a8c8cbfeb2455dd056
Varun Govil <varun.govil@trioptima.com>
2021-02-18 17:41:57 +0100

    update dependencies, probably fixes the warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-var path = require('path');
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+var path = require("path");
+var webpack = require("webpack");
+var BundleTracker = require("webpack-bundle-tracker");
 module.exports = {
-    entry:  {
-        "app1": path.join(__dirname, 'assets/src/js/index'),
-        "app2": path.join(__dirname, 'assets/src/js/index2'),
-    },
-    output: {
-        path: path.join(__dirname, 'assets/dist'),
-        filename: '[name]-[hash].js'
-    },
-    plugins: [
-        new BundleTracker({
-            path: __dirname,
-            filename: 'webpack-stats.json'
-        }),
+  entry: {
+    app1: path.join(__dirname, "assets/src/js/index"),
+    app2: path.join(__dirname, "assets/src/js/index2"),
+  },
+  output: {
+    path: path.join(__dirname, "assets/dist"),
+    filename: "[name]-[hash].js",
+  },
+  plugins: [
+    new BundleTracker({
+      path: __dirname,
+      filename: "webpack-stats.json",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-}
+  },
+};
